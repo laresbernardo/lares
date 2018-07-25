4aaa0e123fd79c6c1c1a5ee013bcd1cfa594cbdc
bernardolares <bernardo.lares@comparamejor.com>
2018-07-25 15:14:13 -0500

    Typo fix on token_dir

--- R/stocks_report.R
+++ R/stocks_report.R
@@ -416,7 +416,7 @@ stocks_report <- function(wd = "personal", cash_fix = 0, creds = NA) {
                           "myportfolio.csv",
                           "mydaily.csv"),
            to = "laresbernardo@gmail.com", 
-           from = 'RServer <bernardo.lares@comparamejor.com>', creds = creds)
+           from = 'RServer <bernardo.lares@comparamejor.com>', creds = token_dir)
   message("4. Email sent. DONE!")
 
   # Clear all out
