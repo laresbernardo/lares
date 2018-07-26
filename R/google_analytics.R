99e9892f075b58a0266e7e3ee08ae44114f4c3f2
bernardolares <bernardo.lares@comparamejor.com>
2018-07-26 17:18:33 -0500

    One more test

--- R/google_analytics.R
+++ R/google_analytics.R
@@ -3,26 +3,27 @@ ga_query <- function(account = "comparamejor",
                      creds = NA,
                      metrics = "sessions",
                      dimensions = "date",
-                     start = as.character(lubridate::floor_date(Sys.Date(), "month")), 
-                     end = as.character(Sys.Date())) {
-  
+                     start = lubridate::floor_date(Sys.Date(), "month"), 
+                     end = Sys.Date()) {
+  message("1")
   account <- paste("google_analytics", account, sep="_")
   message(paste("Account:", account))
-  
+  message("2")
   require(googleAuthR)
   require(googleAnalyticsR)
+  message("3")
   vars <- lares::get_credentials(from = account, dir = creds)
   ga_id <- vars$ga_id
+  gar_auth()
   
   return(
-    google_analytics(
-    ga_id, 
-    start = start,
-    end = end,
-    metrics = metrics,
-    dimensions = dimensions)
+    googleAnalyticsR::google_analytics(
+      ga_id, 
+      date_range = c(start, end),
+      metrics = metrics,
+      dimensions = dimensions)
   )
   
   googleAuth(revoke = TRUE)
-
+  
 }
