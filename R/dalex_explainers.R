58d30bc78de18d04d31f20d2806aa952e9cb46d5
Bernardo <laresbernardo@gmail.com>
2018-08-01 10:11:56 -0500

    Added option to create custom pred function

--- R/dalex_explainers.R
+++ R/dalex_explainers.R
@@ -1,14 +1,17 @@
 ############## DALEX explainer ############## 
-dalex_explainer <- function(df, model, label = "lares::h2o_automl") {
+dalex_explainer <- function(df, model, model_name = "lares::h2o_automl", pred = NA) {
   require(DALEX) 
   
   x_valid <- as.data.frame(df) %>% select(-tag)
   y_valid <- as.integer(df$tag)
   
-  pred <- function(model, newdata)  {
-    h2o.no_progress()
-    results <- as.data.frame(h2o.predict(model, as.h2o(newdata)))
-    return(results[[3L]])
+  # Prediction function
+  if (is.na(pred)) {
+    pred <- function(model, newdata)  {
+      h2o.no_progress()
+      results <- as.data.frame(h2o.predict(model, as.h2o(newdata)))
+      return(results[[3L]])
+    }
   }
   
   explainer <- explain(
@@ -16,11 +19,11 @@ dalex_explainer <- function(df, model, label = "lares::h2o_automl") {
     data = x_valid,
     y = y_valid,
     predict_function = pred,
-    label = label
+    label = model_name
   )
   
   return(explainer)
-
+  
 }
 # explainer <- dalex_explainer(results$datasets$train, results$model)
 
@@ -42,7 +45,7 @@ dalex_local <- function(explainer, row, plot = TRUE, print = TRUE) {
   }
   
   return(breakdown)
-
+  
 }
 # local <- dalex_local(explainer, results$datasets$train[15,], plot = TRUE, print = FALSE)
 
