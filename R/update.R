29d5ef1d1d190e43da66c670422eafdc1a36a882
bernardolares <bernardo.lares@comparamejor.com>
2018-08-02 12:07:16 -0500

    Force update option

--- R/update.R
+++ R/update.R
@@ -4,8 +4,9 @@
 #' This function lets the user update from repository or local source.
 #'
 #' @param local Boolean. Install package with local files (TRUE) or Github repository
+#' @param local Boolean. Force install if needed
 #' @export
-updateLares <- function(local = FALSE) {
+updateLares <- function(local = FALSE, force = FALSE) {
   
   suppressMessages(require(devtools))
   suppressMessages(require(config))
@@ -16,7 +17,7 @@ updateLares <- function(local = FALSE) {
   if (local == TRUE) {
     devtools::install("~/Dropbox (Personal)/Documentos/R/Github/lares")
   } else {
-    devtools::install_github("laresbernardo/lares") 
+    devtools::install_github("laresbernardo/lares", force = force) 
   }
   
   message(paste(Sys.time(), "| Duration:", round(difftime(Sys.time(), start, units="secs"), 2), "s"))
