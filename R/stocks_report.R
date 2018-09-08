fd1492c363173052f9259bd88f7052b2ffa718af
Bernardo <laresbernardo@gmail.com>
2018-09-07 19:55:29 -0500

    xgg

--- R/stocks_report.R
+++ R/stocks_report.R
@@ -10,6 +10,7 @@ get_stocks <- function(token_dir="~/Dropbox (Personal)/Documentos/Interactive Br
   suppressMessages(require(rdrop2))
   suppressMessages(require(xlsx))
   suppressMessages(require(dplyr))
+  suppressMessages(require(lubridate))
   options("getSymbols.yahoo.warning"=FALSE)
 
   valid <- Sys.info()
@@ -29,7 +30,7 @@ get_stocks <- function(token_dir="~/Dropbox (Personal)/Documentos/Interactive Br
     cash <- read.xlsx(file, sheetName = 'Fondos', header=TRUE, colClasses=NA)
     trans <- read.xlsx(file, sheetName = 'Transacciones', header=TRUE, colClasses=NA)
     port <- read.xlsx(file, sheetName = 'Portafolio', header=TRUE, colClasses=NA)
-    symbols <- data.frame(Symbols = as.character(port$Symbol), StartDate = as.Date(port$Date))
+    symbols <- data.frame(Symbols = as.character(port$Symbol), StartDate = date(port$Date))
     port <- data.frame(cbind(port, StartDate = symbols$StartDate))
 
     results <- list("portfolio" = port, "transactions" = trans, "cash" = cash)
