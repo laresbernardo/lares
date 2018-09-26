0ef178f236dd5c665191667bfeed88df0fb65897
bernardolares <bernardo.lares@comparamejor.com>
2018-09-26 17:21:33 -0500

    lkasmvñmwa

--- R/stocks_report.R
+++ R/stocks_report.R
@@ -238,8 +238,9 @@ stocks_performance <- function(dailys, cash_in, cash_fix = 0)  {
     mutate(DailyCash = ifelse(is.na(Cash), 0, Cash),
            CumCash = cumsum(DailyCash) - cumsum(DailyTrans) + cumsum(DailyDiv) + cash_fix,
            CumPortfolio = CumCash + DailyStocks,
+           TotalUSD = cumsum(DailyTrans) - DailyStocks,
            TotalPer = round(100 * DailyStocks / (cumsum(DailyTrans)), 2) - 100) %>%
-    select(Date,CumPortfolio,TotalPer,RelUSD,RelPer,DailyStocks,
+    select(Date,CumPortfolio,TotalUSD,TotalPer,RelUSD,RelPer,DailyStocks,
            DailyTrans,DailyDiv,CumDiv,DailyCash,CumCash) %>% arrange(desc(Date)) %>%
     mutate_if(is.numeric, funs(round(., 2)))
 
@@ -316,7 +317,8 @@ portfolio_daily_plot <- function(stocks_perf) {
     labs(y = '% Daily Var', x = '',
          title = 'Daily Portfolio\'s Stocks Change (%) since Start',
          subtitle = paste(stocks_perf$Date[1]," (Includes Expenses): ",
-                          stocks_perf$TotalPer[1],"% ($",lares::formatNum(stocks_perf$DailyStocks[1] - sum(stocks_perf$DailyTrans),0),") | $",
+                          lares::formatNum(stocks_perf$TotalPer[1],2),"% ($",
+                          lares::formatNum(stocks_perf$DailyStocks[1] - sum(stocks_perf$DailyTrans),0),") | $",
                           lares::formatNum(stocks_perf$CumPortfolio[1]), sep="")) +
     ggsave("portf_daily_change.png", width = 8, height = 5, dpi = 300)
 
@@ -348,7 +350,7 @@ stocks_total_plot <- function(stocks_perf, portfolio_perf, daily, trans, cash) {
     paste0("Stocks: $", lares::formatNum(sum(stocks_perf$DailyStocks[1]))," & Cash: $", stocks_perf$CumCash[1]),
     paste0("Stocks Investment: $", lares::formatNum(sum(trans$Amount,na.rm=T))),
     paste0("ROI: ", lares::formatNum(stocks_perf$TotalPer[1], 2),"% ($",
-           lares::formatNum(stocks_perf$DailyStocks[1] - sum(stocks_perf$DailyTrans),1),")"),
+           lares::formatNum(stocks_perf$TotalUSD[1],0),")"),
     paste0("Dividends: $", lares::formatNum(sum(daily$DailyDiv),0)," & Expenses: $", 
            lares::formatNum(sum(daily$Expenses),0)))
   
