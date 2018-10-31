188e376927ba08ad2722991943948974888e39d6
Bernardo <laresbernardo@gmail.com>
2018-10-30 23:16:49 -0500

    Improving docs

--- R/lares.R
+++ R/lares.R
@@ -1,8 +1,5 @@
 #' lares: Analytics, Visualization & Machine Learning Library
 #'
-#' To start using lares you can start by the vignette:
-#' `browseVignettes(package = "lares")`
-#' 
 
 #' @importFrom beepr beep
 #' @importFrom config get
@@ -19,8 +16,7 @@
 #' @importFrom gridExtra grid.arrange
 #' @importFrom h2o as.h2o h2o.automl h2o.accuracy h2o.getModel h2o.performance h2o.init h2o.removeAll
 #' @importFrom kableExtra kable
-#' @importFrom lubridate date day week weeks month year wday dmy_hms dmy ymd_hms ymd setdiff minute hour
-#' @importFrom mailR send.mail
+#' @importFrom lubridate date day week weeks month year wday dmy_hms dmy ymd_hms ymd minute hour
 #' @importFrom openxlsx addWorksheet copyWorkbook loadWorkbook read.xlsx removeWorksheet renameWorksheet saveWorkbook sheets write.xlsx
 #' @importFrom pROC roc ci
 #' @importFrom RColorBrewer brewer.pal
@@ -30,11 +26,14 @@
 #' @import rlist   
 #' @importFrom rtweet create_token search_tweets
 #' @importFrom scales percent comma
-#' @importFrom stats cor quantile
-#' @importFrom stats na.omit sd median
-#' @import SnowballC
-#' @importFrom utils head tail packageVersion
+#' @importFrom stats cor quantile complete.cases
+#' @importFrom stats na.omit sd median dist end lm predict reorder start
+#' @import SnowballC 
+#' @importFrom utils head tail packageVersion URLencode capture.output data download.file installed.packages write.table
 #' @importFrom wordcloud textplot wordlayout
+#' @importFrom datasets trees
+#' @importFrom grDevicesdev.off graphics.off
+#' @importFrom graphics box hist plot points rasterImage rect
 "_PACKAGE"
 
 utils::globalVariables(names=c(
@@ -43,5 +42,6 @@ utils::globalVariables(names=c(
   "guides","labs","theme","element_text","scale_y_continuous","position_dodge","ylim","guide_legend","scale_fill_discrete",
   "aes_string", "geom_boxplot","stat_summary", "theme_minimal", "theme_bw", "geom_vline", "geom_density", "margin",
   "scale_colour_continuous",'label','coord_flip','ylab','xlab','labs','geom_label','unit',
-  'geom_line','geom_point','xlim','ylim','geom_segment','.','one_of',"head","tail", "type", "cuts"), 
+  'geom_line','geom_point','xlim','ylim','geom_segment','.','one_of',"head","tail", "type", "cuts",
+  'gather','value','key'), 
   package = "lares", add = F)
