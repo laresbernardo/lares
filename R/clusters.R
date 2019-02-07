ed6c8cd667432cade0c743a232048eb676b44ffa
Bernardo <laresbernardo@gmail.com>
2019-02-07 10:32:51 -0300

    Added centers in plots for kmeans

--- R/clusters.R
+++ R/clusters.R
@@ -5,24 +5,33 @@
 #' If needed, one hot encoding will be applied to categorical values.
 #' 
 #' @param df Dataframe
-#' @param force_n Integer. Number of clusters
+#' @param k Integer. Number of clusters
 #' @param limit Integer. How many clusters should be considered?
 #' @param ohse Boolean. Do you wish to automatically run one hot
 #' encoding to non-numerical columns?
-#' @param comb Vector. Which columns do you wish to plot? Select which 2
-#' by name or position.
+#' @param norm Boolean. Should the data be normalized?
+#' @param comb Vector. Which columns do you wish to plot? Select which
+#' two variables by name or column position.
 #' @export
-clusterKmeans <- function(df, force_n = NA, limit = 20, ohse = TRUE, comb = c(1,2)){
+clusterKmeans <- function(df, k = NA, limit = 20, ohse = TRUE, norm = TRUE, comb = c(1,2)){
+  
+  results <- list()
   
   # Only numerical values
-  if (ohse) {
-    df <- ohse(df)
+  nums <- df_str(df, return = "names", plot = F)$nums
+  if (ohse & length(nums) != ncol(df)) {
+    df <- ohse(df, redundant = TRUE, dates = TRUE, limit = 6)
     message("One hot encoding applied...")
   } else {
     df <- data.frame(df) %>% select_if(is.numeric)
   }
   
-  results <- list()
+  # Data should be normalized for better results
+  if (norm) {
+    df <- df %>% transmute_all(funs(normalize))
+  }
+  
+  results[["df"]] <- df
   
   # Determine number of clusters (n)
   wss <- sum(apply(df, 2, var))*(nrow(df)-1)
@@ -39,29 +48,34 @@ clusterKmeans <- function(df, force_n = NA, limit = 20, ohse = TRUE, comb = c(1,
   results[["nclusters_plot"]] <- nclusters_plot
   
   # If n is already selected
-  if (!is.na(force_n)) {
+  if (!is.na(k)) {
     nclusters_plot <- nclusters_plot + 
-      geom_hline(aes(yintercept = nclusters$wss[nclusters$n==force_n]), colour = "red") +
-      labs(subtitle = paste("Number of clusters selected:", force_n))
-    results[["clusters"]] <- force_n
+      geom_hline(aes(yintercept = nclusters$wss[nclusters$n==k]), colour = "red") +
+      labs(subtitle = paste("Number of clusters selected:", k))
+    results[["clusters"]] <- k
     results[["nclusters_plot"]] <- nclusters_plot
     
     # K-Means Cluster Analysis
     set.seed(21132)
-    fit <- kmeans(df, force_n)
+    fit <- kmeans(df, k)
     # Append cluster assignment
     df <- data.frame(df, cluster = as.factor(fit$cluster))
     # Get cluster means
-    results[["clusters"]] <- df %>% 
+    clusters <- df %>% 
       group_by(cluster) %>% 
       summarise_all(list(mean)) %>%
       mutate(n = as.integer(table(df$cluster)))
+    results[["clusters"]] <- clusters
     # Plot clusters
     axisnames <- colnames(df[,comb])
-    clusters_plot <- ggplot(df, aes(x=df[,comb[1]], y=df[,comb[2]], colour = df$cluster)) + 
+    centers <- clusters[,-1][,comb]
+    clusters_plot <- ggplot(df, aes(
+      x=df[,comb[1]], y=df[,comb[2]], colour = df$cluster)) + 
       geom_point() + theme_minimal() +
+      geom_point(data=centers, aes_string(x=colnames(centers)[1], y=colnames(centers)[2]), 
+                 shape=18, colour="black") +
       labs(title = "Clusters Plot",
-           subtitle = paste("Number of clusters selected:", force_n),
+           subtitle = paste("Number of clusters selected:", k),
            x = axisnames[2], y = axisnames[1],
            colour = "Cluster")
     results[["clusters_plot"]] <- clusters_plot
