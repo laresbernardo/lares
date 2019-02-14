467bffd10a849616b0baeb3ecd8341fe8ab59ae6
Bernardo <laresbernardo@gmail.com>
2019-02-14 12:33:51 -0300

    New datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,20 +1,38 @@
-#' Random Lares Data for Examples
+####################################################################
+#' Dataset: Titanic Sub-dataset por Examples
+#'
+#' Data generated from a Titanic dataset.
+#'
+#' @docType data
+#' @usage data(dft)
+#' @format An object of class \code{"data.frame"}
+#' @keywords datasets titanic
+#' @examples
+#' data(dft)
+"dft"
+
+# dft <- read.csv("~/Dropbox (Personal)/Documentos/R/R Titanic/train.csv") %>%
+#   select(-Name) %>% mutate(Pclass = factor(Pclass))
+# save(dft, file="data/dft.RData")
+
+
+
+####################################################################
+#' Dataset: Random Data for Examples
 #'
 #' Data randomly generated for examples.
 #'
 #' @docType data
 #' @usage data(dfl)
 #' @format An object of class \code{"data.frame"}
-#' @keywords datasets
+#' @keywords datasets random
 #' @examples
 #' data(dfl)
-#' df_str(dfl)
 "dfl"
 
 # library(lares)
 # library(dplyr)
-# 
-# df <- queryDummy(
+# dfl <- queryDummy(
 #   "SELECT random() as rand,
 #   opp_id, opp_date, funnel_kind, motorcycle, 
 #   policy_price, total_discount as promo, issued
@@ -22,19 +40,9 @@
 #   WHERE policy_price IS NOT NULL
 #   AND date(opp_date) >= CURRENT_DATE - INTERVAL '30 days'
 #   ORDER BY rand
-#   LIMIT 1000")
-# dfl <- df %>% 
+#   LIMIT 1000") %>%
 #   mutate(novar = "soat") %>%
 #   mutate(kind = factor(ifelse(right(opp_id, 1) %in% c(1,5), NA, funnel_kind))) %>%
 #   rename(values = policy_price, type = motorcycle) %>%
-#   select(-rand, -funnel_kind)
-# df_str(dfl, return = "plot")
-# head(dfl)
-# 
-# dfl %>% corr_var(issued, top = 10)
-# dfl %>% distr(issued, promo)
-# dfl %>% distr(issued, kind)
-# dfl %>% distr(issued, values, abc=T)
-# dfl %>% distr(issued, type, abc=T)
-# 
+#   select(-rand, -funnel_kind) %>% 
 # save(dfl, file="data/dfl.RData")
