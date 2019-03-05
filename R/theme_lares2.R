33dc158d2b61350f1f90766bf5198b0b9d4fad13
Bernardo <laresbernardo@gmail.com>
2019-03-05 17:43:09 -0300

    Transparency for theme_lares2() and added pipe fx

--- R/theme_lares2.R
+++ R/theme_lares2.R
@@ -24,25 +24,28 @@ theme_lares2 <- function(font = "Arial Narrow", base_size = 11.5,
                          second_colour = "deepskyblue3",
                          soft_colour = "grey30") {
   
+  # Start from theme_minimal()
   ret <- theme_minimal(base_family = font, base_size = base_size)
   
-  ret <- ret + theme(plot.title = element_text(color = "black"),
-                     plot.caption = element_text(color = soft_colour, face = "bold"),
-                     legend.title = element_text(color = soft_colour, size = base_size * 0.9, face = "bold"),
-                     axis.line=element_blank(),
-                     text = element_text(family = font))
+  # Set default font
+  ret <- ret + theme(text = element_text(family = font))
   
+  # Set some defaults
   update_geom_defaults("text", list(family = font))
   update_geom_defaults("label", list(family = font))
   #update_geom_defaults("text_repel", list(family = font))
-  update_geom_defaults("point", list(colour = main_colour))
-  update_geom_defaults("bar", list(fill = main_colour))
-  update_geom_defaults("col", list(fill = main_colour))
+  update_geom_defaults("point", list(colour = main_colour, alpha = 0.95))
+  update_geom_defaults("bar", list(fill = main_colour, alpha = 0.95))
+  update_geom_defaults("col", list(fill = main_colour, alpha = 0.95))
+  update_geom_defaults("boxplot", list(fill = main_colour, alpha = 0.95))
+  update_geom_defaults("density", list(fill = main_colour, alpha = 0.95))
   
+  # Edit some functions
   scale_y_continuous <- function(...) ggplot2::scale_y_continuous(..., labels = scales::comma)
   scale_x_continuous <- function(...) ggplot2::scale_x_continuous(..., labels = scales::comma)
   scale_colour_continuous <- function(...) ggplot2::scale_colour_gradient(
     low = second_colour, high = main_colour, na.value = soft_colour, ...)
+  ggsave <- function(...) ggplot2::ggsave(..., bg = "transparent")
   
   if (inherits(grid, "character")) {
     grid_col <- "#CCCCCC"
@@ -60,12 +63,18 @@ theme_lares2 <- function(font = "Arial Narrow", base_size = 11.5,
   xj <- switch(tolower(substr("left", 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
   yj <- switch(tolower(substr("left", 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
   
-  ret <- ret + theme(
-    axis.ticks = element_blank(), axis.ticks.x = element_blank(), axis.ticks.y = element_blank())
+  # Axis lines
+  ret <- ret + theme(axis.line = element_blank())
+  # Axis ticks
+  ret <- ret + theme(axis.ticks = element_blank(), 
+                     axis.ticks.x = element_blank(), 
+                     axis.ticks.y = element_blank())
+  # Axis text
   ret <- ret + theme(axis.text.x=element_text(
     size=base_size * 0.85, margin=margin(t=0),colour = soft_colour))
   ret <- ret + theme(axis.text.y=element_text(
     size=base_size * 0.85, margin=margin(r=0),colour = soft_colour))
+  # Axis titles
   ret <- ret + theme(axis.title=element_text(
     size=base_size * 0.85, family=font,colour = soft_colour))
   ret <- ret + theme(axis.title.x=element_text(
@@ -73,21 +82,32 @@ theme_lares2 <- function(font = "Arial Narrow", base_size = 11.5,
   ret <- ret + theme(axis.title.y=element_text(
     hjust=yj, size=base_size * 0.85, colour = soft_colour,family=font, face="bold"))
   ret <- ret + theme(axis.title.y.right=element_text(
-    hjust=yj, size=base_size * 0.85, angle=-90,colour = soft_colour,
-    family=font, face="bold"))
+    hjust=yj, size=base_size * 0.85, angle=-90,colour = soft_colour,family=font, face="bold"))
+  # facet_grid
   ret <- ret + theme(strip.text=element_text(
     hjust=0, size=base_size * 1, colour = soft_colour, face="bold", family=font))
   ret <- ret + theme(panel.spacing=grid::unit(2, "lines"))
+  # Plot title
   ret <- ret + theme(plot.title=element_text(
     hjust=0, size=base_size * 1.45, margin=margin(b=base_size * 0.85), 
-    family=font, face="bold"))
+    family=font, face="bold", color = "black"))
+  # Plot subtitle
   ret <- ret + theme(plot.subtitle=element_text(
     hjust=0, size=base_size * 1, colour = soft_colour, 
     margin=margin(b=base_size * 0.7),family = font, face="italic"))
+  # Caption
   ret <- ret + theme(plot.caption=element_text(
     hjust=1, size=base_size * 0.85, margin = margin(t=base_size * 0.85), 
-    family=font, face="plain"))
+    family=font, face="bold", color = soft_colour))
+  # Legend 
+  ret <- ret + theme(legend.title = element_text(
+    color = soft_colour, size = base_size * 0.9, face = "bold"))
+  # External margins
   ret <- ret + theme(plot.margin = margin(15, 15, 15, 15))
+  # Background
+  ret <- ret + theme(
+    panel.background = element_rect(fill = "transparent", colour = NA),
+    plot.background = element_rect(fill = "transparent", colour = NA))
   
   return(ret)
   
