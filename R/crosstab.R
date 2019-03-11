6780ae300b046a28aae4ef9481ca2958e4bc7c71
Bernardo <laresbernardo@gmail.com>
2019-03-11 20:04:52 -0300

    Added order option for crosstab

--- R/crosstab.R
+++ R/crosstab.R
@@ -5,12 +5,13 @@
 #' 
 #' @param x,y Vectors. For dependent and independent values
 #' @param weight Vector. An optional vector for a weighted cross tabulation
+#' @param order Boolean. Sort desc the whole table?
 #' @param prow,pcol,pall Boolean. Calculate percent values for rows, columns,
 #' or the whole table, respectively.
 #' @param decimals Integer. How many decimals should be returned?
 #' @param keep_nas Boolean. Keep NAs and count them as well?
 #' @export
-crosstab <- function(x, y, weight = 1, 
+crosstab <- function(x, y, weight = 1, order = TRUE,
                      prow = FALSE, pcol = FALSE, pall = FALSE, 
                      decimals = 2, keep_nas = TRUE) {
   
@@ -20,20 +21,27 @@ crosstab <- function(x, y, weight = 1,
   }
   
   if (length(weight) == 1) {
-    weight <- rep(as.numeric(weight), length(x))
+    weight <- rep(weight, length(x))
   }
   
   weight <- as.numeric(weight)
   t <- round(xtabs(weight ~ x + y))
   tt <- data.frame(t) %>% 
     tidyr::spread(y, Freq) %>% .[,-1] %>%
-    mutate(total = rowSums(.)) %>%
+    mutate(n = rowSums(.)) %>%
     t() %>% data.frame() %>% 
     mutate_all(function(x) as.numeric(as.character(x))) %>%
-    mutate(Total = rowSums(.)) %>%
+    mutate(n = rowSums(.)) %>%
     t() %>% data.frame() %>% 
     mutate_all(function(x) as.numeric(as.character(x)))
   ret <- tt
+  
+  rows <- data.frame(name = c(rownames(t),"Total"), n = as.vector(unlist(tt[ncol(tt)]))) %>% 
+    arrange(desc(n)) %>% mutate(rank = row_number()) %>%
+    mutate(rank = ifelse(name == "Total", nrow(tt)+1, rank)) %>% arrange(rank)
+  cols <- data.frame(name = c(colnames(t),"Total"), n = as.vector(unlist(tt[nrow(tt),]))) %>% 
+    arrange(desc(n)) %>% mutate(rank = row_number()) %>%
+    mutate(rank = ifelse(name == "Total", ncol(tt)+1, rank)) %>% arrange(rank)
   
   # pall
   if (pall) {
@@ -58,5 +66,11 @@ crosstab <- function(x, y, weight = 1,
   
   colnames(ret) <- c(colnames(t), "Total")
   rownames(ret) <- c(rownames(t) ,"Total")
+  
+  if (order) {
+    ret <- ret[rows$name, cols$name]
+  }
+  
   return(ret)
+  
 }
