621ade06fe9a4872ff3a05c8a3546f64322c4ea3
Bernardo <laresbernardo@gmail.com>
2019-03-14 23:02:22 -0300

    Palettes reworked

--- R/colour_palettes.R
+++ R/colour_palettes.R
@@ -1,9 +1,148 @@
 ####################################################################
+#' Personal Colours Palette
+#' 
+#' This function plots a list of colours on a specific palette
+#' 
+#' @export
+lares_pal <- function() {
+  
+  black <- "#000000"
+  white <- "#FFFFFF"
+  red <- "tomato"
+  green <- "#3DA4AB" 
+  pink <- "lightpink2"
+  blue <- "#0E9AA7"
+  orange <- "#FE8A71"
+  grey <- "azure4"
+  
+  colours_list <- data.frame(rbind(
+    c("allianz", "#0038A8", black),
+    c("equidad", "#52CF44", black),
+    c("colpatria", "#EE0606", black),
+    c("del estado", "#F37000", black),
+    c("suramericana", "#1F6D8C", black),
+    c("mapfre", "#34000D", black),
+    c("la previsora", "#6F9A45", black),
+    c("aig", "#C71585", black),
+    c("generali", "#B21F1F", black),
+    c("solidaria", "#E69500", black),
+    c("liberty", "#4E629A", black),
+    c("bolivar", "#F0F206", black),
+    c("cia", "#8ACBE5", black),
+    c("mundial", "#8ACBE5", black),
+    c("puntored", "#FFFF00", grey),
+    c("movilred", "#FF1493", black),
+    c("moviired", "#FF1493", black),
+    c("web", "#290452", white),
+    c("somosf1", "#290452", white),
+    c("f1", "#290452", white),
+    c("funnel-soat4_desktop", "#290452", white),
+    c("funnel-ujk2d_desktop", "#8ACBE5", black),
+    c("funnel-ujk2m_old_mobile", "#7AC4E1", black),
+    c("red", black, black),
+    c("m", blue, white),
+    c("f", pink, black),
+    c("male", blue, white),
+    c("female", pink, black),
+    c("hombre", blue, white),
+    c("mujer", pink, black),
+    c("true", green, white),
+    c("false", red, black),
+    c("TRUE", green, white),
+    c("FALSE", red, black),
+    c("1", green, white),
+    c("0", red, black),
+    c("good", green, white),
+    c("bad", red, black),
+    c("bueno", green, white),
+    c("malo", red, black),
+    c("spring", green, white),
+    c("summer", red, white),
+    c("fall", orange, black),
+    c("winter", blue, black),
+    c("meg1", "071D49", white),
+    c("meg2", "EBB600", black),
+    c("meg3", "F2F1F0", black),
+    c("meg4", "9A9A9A", white)
+  ))
+  colnames(colours_list) <- c("values","fill","colour")
+  
+  colours_names <- c(
+    "#EBB600" = black,
+    "#40A4D8" = black,
+    "#5D3A9B" = white,
+    "#DC3220" = black,
+    "#D35FB7" = black,
+    "#32CD32" = black,
+    "#2FC9FE" = black,
+    "#F29595" = black,
+    "#2FFECC" = black,
+    "#290452" = white,
+    "#0C7BDC" = black,
+    "#817B7B" = black,
+    "#F66320" = black,
+    "#FEFE62" = black,
+    "#005AB5" = white,
+    "#9A9A9A" = black,
+    "#00008B" = white,
+    "#E1BE6A" = black,
+    "#40B0A6" = black,
+    "#7ADf90" = black,
+    "#056E00" = white,
+    "#E40000" = black,
+    "#FE8A71" = black,
+    "#8600A1" = white,
+    "#A52A2A" = white,
+    "#000000" = white)
+  
+  pal <- list(labels = colours_list, 
+              palette = colours_names)
+  return(pal)
+}
+
+# plot_palette(names(colours_names), colours_names)
+# plot_palette(colours_list$fill, colours_list$colour, colours_list$values)
+
+####################################################################
+#' Plot Palette Colours
+#' 
+#' This function plots a list of colours
+#' 
+#' @param fill Vector. List of colours for fills
+#' @param colour Vector. List of colours for colours
+#' @param id Vector. ID for each color
+#' @export
+plot_palette <- function(fill, colour = "black", id = NA) {
+  
+  options(warn=-1)
+  
+  if (is.na(id)) {
+    id <- 1:length(fill)
+  }
+  
+  data.frame(fill = fill, 
+             colour = colour,
+             id = id) %>%
+    ggplot(aes(x=reorder(fill, -id), y=1)) + 
+    geom_bar(aes(fill = fill), stat="identity", position="dodge") +
+    geom_text(aes(colour = colour, label=id), hjust = 1.5) +
+    scale_fill_identity() +
+    scale_colour_identity() +
+    coord_flip() + labs(x = "Colours", y = "") +
+    guides(fill=FALSE, colour=FALSE) +
+    theme_lares2() + 
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())
+}
+
+
+####################################################################
 #' lares Theme for ggplot2
 #' 
 #' This function sets some default values into ggplot2 outputs. This
 #' function is almost as using gg_colour_customs() + gg_fill_customs() 
-#' + gg_text_customs, but these won't be mantained any longer.
+#' + gg_text_customs = but these won't be mantained any longer.
 #' 
 #' @param labels Boolean. Add labels to plot?
 #' @param colours Boolean. Personalize colour palettes?
@@ -28,70 +167,8 @@ theme_lares <- function(labels = FALSE, colours = TRUE, cont = FALSE,
   }
   
   if (colours) {
-    red <- "tomato"
-    green <- "#3DA4AB" 
-    pink <- "lightpink2"
-    blue <- "#0E9AA7"
-    orange <- "#FE8A71"
-    black <- "#000000"
-    white <- "#FFFFFF"
-    grey <- "azure4"
-    megblue <- "#071D49"
-    megyellow <- "#EBB600"
-    meggrey1 <- "#F2F1F0"
-    meggrey2 <- "#9A9A9A"
     
-    colours_list <- data.frame(rbind(
-      c("allianz", "#0038A8", black),
-      c("equidad", "#52CF44", black),
-      c("colpatria", "#EE0606", black),
-      c("del estado", "#F37000", black),
-      c("suramericana", "#1F6D8C", black),
-      c("mapfre", "#34000D", black),
-      c("la previsora", "#6F9A45", black),
-      c("aig", "#C71585", black),
-      c("generali", "#B21F1F", black),
-      c("solidaria", "#E69500", black),
-      c("liberty", "#4E629A", black),
-      c("bolivar", "#F0F206", black),
-      c("cia", "#8ACBE5", black),
-      c("mundial", "#8ACBE5", black),
-      c("puntored", "#FFFF00", grey),
-      c("movilred", "#FF1493", black),
-      c("moviired", "#FF1493", black),
-      c("web", "#290452", white),
-      c("somosf1", "#290452", white),
-      c("f1", "#290452", white),
-      c("funnel-soat4_desktop", "#290452", white),
-      c("funnel-ujk2d_desktop", "#8ACBE5", black),
-      c("funnel-ujk2m_old_mobile", "#7AC4E1", black),
-      c("red", grey, black),
-      c("m", blue, white),
-      c("f", pink, black),
-      c("male", blue, white),
-      c("female", pink, black),
-      c("hombre", blue, white),
-      c("mujer", pink, black),
-      c("true", green, black),
-      c("false", red, black),
-      c("TRUE", green, black),
-      c("FALSE", red, black),
-      c("1", green, black),
-      c("0", red, black),
-      c("good", green, black),
-      c("bad", red, black),
-      c("bueno", green, black),
-      c("malo", red, black),
-      c("spring", green, white),
-      c("summer", red, white),
-      c("fall", orange, black),
-      c("winter", blue, black),
-      c("meg1", megblue, white),
-      c("meg2", megyellow, black),
-      c("meg3", meggrey1, black),
-      c("meg4", meggrey2, white)
-    ))
-    colnames(colours_list) <- c("values","fill","colour")
+    colours_list <- lares_pal()$labels
     
     scale_fill_lares <- function(){
       values <- as.character(t(colours_list$fill)[1,])
@@ -107,12 +184,10 @@ theme_lares <- function(labels = FALSE, colours = TRUE, cont = FALSE,
     
     if (cont) {
       scale_colour_lares <- function(){
-        # https://www.color-hex.com/color-palette/69673
-        pal_lares <- c("#817b7b", "#0e9aa7", "#3da4ab", "#f6cd61", "#fe8a71")
+        pal_lares <- lares_pal()$palette
         structure(list(scale_color_gradientn(colours = pal_lares)))
       } 
     }
-    
     r <- c(r, scale_fill_lares(), scale_colour_lares())
   }
   
@@ -129,49 +204,10 @@ theme_lares <- function(labels = FALSE, colours = TRUE, cont = FALSE,
 #' @export
 gg_colour_customs <- function () {
   
-  red <- "tomato"
-  green <- "mediumspringgreen"
-  pink <- "lightpink2"
-  blue <- "steelblue2"
-  orange <- "orange"
+  colours_list <- lares_pal()$labels
+  values <- as.character(t(colours_list$colour)[1,])
+  names(values) <- colours_list$values
   
-  values <- c("allianz" = "#0038A8",
-              "equidad" = "#52CF44",
-              "colpatria" = "#EE0606",
-              "del estado" = "#F37000",
-              "suramericana" = "#1F6D8C",
-              "mapfre" = "#34000D",
-              "la previsora" = "#6F9A45",
-              "aig" = "#C71585",
-              "generali" = "#B21F1F",
-              "solidaria" = "#E69500",
-              "liberty" = "#4E629A",
-              "bolivar" = "#F0F206",
-              "cia" = "#8ACBE5",
-              "puntored" = "#FFFF00",
-              "movilred" = "#FF1493",
-              "web" = "#290452",
-              "funnel-soat4_desktop" = "#290452",
-              "red" = "azure4",
-              "f1" = "#290452",
-              "m" = blue,
-              "f" = pink,
-              "true" = green,
-              "false" = red,
-              "TRUE" = green,
-              "FALSE" = red,
-              "1" = green,
-              "0" = red,
-              "good" = green,
-              "bad" = red,
-              "bueno" = green,
-              "malo" = red,
-              "buenos" = green,
-              "malos" = red,
-              "spring" = green,
-              "summer" = red,
-              "fall" = orange,
-              "winter" = blue)
   return(scale_color_manual(values = values))
 }
 
@@ -184,49 +220,10 @@ gg_colour_customs <- function () {
 #' @export
 gg_fill_customs <- function () {
   
-  red <- "tomato"
-  green <- "mediumspringgreen"
-  pink <- "lightpink2"
-  blue <- "steelblue2"
-  orange <- "orange"
+  colours_list <- lares_pal()$labels
+  values <- as.character(t(colours_list$fill)[1,])
+  names(values) <- colours_list$values
   
-  values <- c("allianz" = "#0038A8",
-              "equidad" = "#52CF44",
-              "colpatria" = "#EE0606",
-              "del estado" = "#F37000",
-              "suramericana" = "#1F6D8C",
-              "mapfre" = "#34000D",
-              "la previsora" = "#6F9A45",
-              "aig" = "#C71585",
-              "generali" = "#B21F1F",
-              "solidaria" = "#E69500",
-              "liberty" = "#4E629A",
-              "bolivar" = "#F0F206",
-              "cia" = "#8ACBE5",
-              "puntored" = "#FFFF00",
-              "movilred" = "#FF1493",
-              "web" = "#290452",
-              "funnel-soat4_desktop" = "#290452",
-              "red" = "azure4",
-              "f1" = "#290452",
-              "m" = blue,
-              "f" = pink,
-              "true" = green,
-              "false" = red,
-              "TRUE" = green,
-              "FALSE" = red,
-              "1" = green,
-              "0" = red,
-              "good" = green,
-              "bad" = red,
-              "bueno" = green,
-              "malo" = red,
-              "buenos" = green,
-              "malos" = red,
-              "spring" = green,
-              "summer" = red,
-              "fall" = orange,
-              "winter" = blue)
   return(scale_fill_manual(values = values))
 }
 
@@ -237,114 +234,11 @@ gg_fill_customs <- function () {
 #' This function lets the user use pre-defined default colours
 #' 
 #' @export
-gg_text_customs <- function () {
+gg_text_customs <- function() {
   
-  black <- "#000000"
-  white <- "#FFFFFF"
+  colours_list <- lares_pal()$labels
+  values <- as.character(t(colours_list$colour)[1,])
+  names(values) <- colours_list$values
   
-  values <- c("allianz" = black,
-              "equidad" = black,
-              "colpatria" = black,
-              "del estado" = black,
-              "suramericana" = black,
-              "mapfre" = black,
-              "la previsora" = black,
-              "aig" = black,
-              "generali" = black,
-              "solidaria" = black,
-              "liberty" = black,
-              "bolivar" = black,
-              "cia" = black,
-              "puntored" = black,
-              "movilred" = black,
-              "web" = white,
-              "funnel-soat4_desktop" = white,
-              "red" = black,
-              "f1" = white,
-              "m" = white,
-              "f" = black,
-              "true" = black,
-              "false" = black,
-              "TRUE"= black,
-              "FALSE"= black,
-              "1" = black,
-              "0" = black,
-              "good" = black,
-              "bad" = black,
-              "bueno" = black,
-              "malo" = black,
-              "spring" = white,
-              "summer" = white,
-              "fall" = black,
-              "winter" = black,
-              "w" = white,
-              "b" = black,
-              "none" = black)
   return(scale_color_manual(values = values))
-}
-
-
-####################################################################
-#' Plot Palette Colours
-#' 
-#' This function plots a list of colours
-#' 
-#' @param vector Vector. List of colours
-#' @export
-plot_palette <- function(vector) {
-  data.frame(colour = vector, 
-             id = 1:length(vector)) %>%
-    ggplot(aes(x=reorder(colour, -id), 
-               y=1, label=id, fill = vector)) + 
-    geom_col() +
-    scale_fill_identity() +
-    geom_text(hjust = -0.5) +
-    coord_flip() + labs(x = "Colours", y = "") +
-    theme_lares2() + guides(fill=FALSE) +
-    theme(axis.title.x=element_blank(),
-          axis.text.x=element_blank(),
-          axis.ticks.x=element_blank())
-}
-
-
-####################################################################
-#' Palette Colours
-#' 
-#' This function plots a list of colours on a specific palette
-#' 
-#' @param which Integer. Which palette?
-#' @export
-lares_pal <- function (which = 1) {
-  
-  # Check palette:
-  # plot_palette(lares_pal(1))
-  
-  if (which == 1) {
-    pal <- c("#40A4D8",
-             "#DC3220",
-             "#EBB600",
-             "#5D3A9B",
-             "#1AFF1A",
-             "#D35FB7",
-             "#2FC9FE",
-             "#2FFECC",
-             "#290452",
-             "#F66320",
-             "#FEFE62",
-             "#005AB5",
-             "lightpink2",
-             "#9A9A9A",
-             "#00008B",
-             "#0C7BDC",
-             "#E1BE6A",
-             "#40B0A6",
-             "#f29595",
-             "#7adf90",
-             "#056e00",
-             "#e40000",
-             "#8600a1",
-             "black",
-             "brown")
-  }
-  return(pal)
 }
