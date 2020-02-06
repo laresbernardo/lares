2da88ef571f1151630fc1e1a98f2eea5244ecb17
Bernardo Lares <bernardolares@fb.com>
2020-02-06 15:03:16 -0300

    New yt2mp3 fx

--- R/lares.R
+++ R/lares.R
@@ -101,7 +101,7 @@ if (getRversion() >= "2.15.1")
       "make_future_dataframe","train_test","aml","m","scores","aggregate_profiles",
       "PC1","PC2","pvalue","ceteris_paribus","contribution","drive_auth","drive_find",
       "udpipe_download_model","udpipe_load_model","udpipe_annotate","keywords_rake",
-      "gs_auth","gs_read","gs_edit_cells","gs_title"))
+      "gs_auth","gs_read","gs_edit_cells","gs_title","av_audio_convert"))
 
 #' Pipe operator
 #' @name lares-exports
