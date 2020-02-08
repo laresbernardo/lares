e8ad311bfb2e9b0fb3d1671c67ee22e2820cfcde
Bernardo <laresbernardo@gmail.com>
2020-02-08 19:24:11 -0300

    Improved yt2mp3 logic

--- R/audio.R
+++ R/audio.R
@@ -7,35 +7,39 @@
 #' @family Scrapper
 #' @family Audio
 #' @param url Characeter. YouTube URL
-#' @param filename Character. Specify output filename. If NA, video's name 
-#' will be automatically used
-#' @param start Numeric. Seeks in the input file to position (Must be 
-#' greater than 0) and crops output
 #' @export
-yt2mp3 <- function(url = "https://www.youtube.com/watch?v=lrlKcCdVw9Q", 
-                   filename = NA, start = 0) {
+yt2mp3 <- function(url = "https://www.youtube.com/watch?v=lrlKcCdVw9Q") {
   
   try_require("av")
   
-  txt <- read_html(url) %>%
-    html_nodes("head title") %>% 
-    html_text() %>% 
-    gsub(" - YouTube", "", .)
-  message("Video: ", txt)
-  filename <- ifelse(is.na(filename), txt, filename)
+  # txt <- read_html(url) %>%
+  #   html_nodes("head title") %>% 
+  #   html_text() %>% 
+  #   gsub(" - YouTube", "", .)
+  # message("Video: ", txt)
+  # filename <- ifelse(is.na(filename), txt, filename)
   
   tryCatch({
-    message(">>> Downloading video...")
-    system(paste("youtube-dl", url, "-o tempvid"))
-    message(">>> Converting to audio...")
-    files <- listfiles(recursive = FALSE)
-    file <- as.character(files$filename[grepl("tempvid", files$filename)])
-    mp3 <- paste0(filename,".mp3")
-    if (file.exists(mp3)) invisible(file.remove(mp3))
-    av_audio_convert(file, mp3, start_time = start) 
-    invisible(file.remove(file))
+    message(">>> Downloading and converting video...")
+    system(paste("youtube-dl",
+                 "-f bestaudio",
+                 "--extract-audio",
+                 "--audio-format mp3",
+                 "--audio-quality 0", 
+                 '-o "%(title)s.%(ext)s"',
+                 url))
+    # message(">>> Converting to audio...")
+    # files <- listfiles(recursive = FALSE)
+    # file <- as.character(files$filename[grepl("tempvid", files$filename)])
+    # mp3 <- paste0(filename,".mp3")
+    # if (file.exists(mp3)) invisible(file.remove(mp3))
+    # av_audio_convert(file, mp3, start_time = start) 
+    # invisible(file.remove(file))
   }, error = function(err) {
-    stop("You need youtube-dl. MacOS: brew install youtube-dl")
+    msg <- "You need to have youtube-dl installed"
+    if (grepl("^darwin", R.version$os))
+      msg <- paste(msg, "Run in Terminal: brew install youtube-dl", sep = "\n")
+    stop(msg)
   })
   message(">>> MP3 created succesfully!")
 }
