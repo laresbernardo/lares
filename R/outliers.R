3410498ff500b5865f981fbfb1b03c56b18bef31
Bernardo Lares <laresbernardo@gmail.com>
2020-07-15 18:05:51 -0300

    No Unicode Character 'MINUS SIGN' (U+2212)

--- R/outliers.R
+++ R/outliers.R
@@ -24,7 +24,7 @@ outlier_zscore <- function(x, thresh = 3, mad = FALSE) {
 #' Outliers: Tukey’s fences
 #' 
 #' Tukey’s fences is a technique used in box plots. The non-outlier 
-#' range is defined with \code{[Q1-k(Q3−Q1), Q3+k(Q3-Q1)]}, where Q1 and Q3 
+#' range is defined with \code{[Q1-k(Q3-Q1), Q3+k(Q3-Q1)]}, where Q1 and Q3 
 #' are the lower and upper quartiles respectively, k - some non-negative 
 #' constant (popular choice is 1.5). A value is an outlier based 
 #' on Tukey’s fences when its value does not lie in non-outlier range.
