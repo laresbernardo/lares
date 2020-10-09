027ef12988e733f53fa090495d90139c178b5316
Bernardo Lares <laresbernardo@gmail.com>
2020-10-09 18:33:41 -0300

    Faster h2o_predict_MOJO

--- R/model_functions.R
+++ R/model_functions.R
@@ -799,43 +799,54 @@ iter_seeds <- function(df, y, tries = 10, ...) {
 #' 
 #' @family Machine Learning
 #' @family Tools
-#' @param df Dataframe. Data to insert into the model
+#' @param df Dataframe. Data to pass to the model.
 #' @param model_path Character. Relative path of directory
-#' where your zip model file is
-#' @param batch Integer. Run n batches at a time
+#' where your zip model file is. If multiple zip files are found, first
+#' one found will be used.
+#' @param method Character. One of "mojo" or "json".
+#' @param batch Integer. Run n batches at a time for "json" method.
 #' @export
-h2o_predict_MOJO <- function(df, model_path, batch = 300){
+h2o_predict_MOJO <- function(df, model_path, method = "mojo", batch = 300){
   
   quiet(h2o.init(nthreads = -1, port = 54321, min_mem_size = "8g"))
   
-  df <- as.data.frame(df)
-  df <- mutate_if(df, is.logical, as.character)
-  file <- listfiles(model_path, regex = ".zip")$filename
-  zip <- paste0(model_path,"/",as.character(file))
+  files <- list.files(model_path)
+  file <- files[endsWith(files, ".zip")][1]
+  zip <- paste0(model_path, "/", file)
   
-  aux <- ceiling(nrow(df)/batch)
-  df$aux <- rep(1:aux, each = batch)[1:nrow(df)]
-  output <- c()
-  for (i in 1:aux) {
-    dfi <- select(df[df$aux == i,], -.data$aux)
-    json <- toJSON(dfi)
-    size <- nchar(json)
-    if (size > 250000)
-      stop(paste("JSON batch is too long. Please, try with a smaller 'batch' parameter.",
-                 "Suggested size:", round(batch * 235000 / size)))
-    res <- h2o.predict_json(zip, json, labels = TRUE)  
-    if ("error" %in% names(res)) {
-      message("\nERROR: There was an issue with one of the inputs to predict...")
-      writeLines(res$error)
-      break
-    }
-    output <- rbind(output, res)
-    if (aux > 1) statusbar(i, aux, i * batch)
+  if (method == "mojo") {
+    df <- quiet(as.h2o(df))
+    mojo_model <- quiet(h2o.import_mojo(zip))
+    output  <- quiet(as.data.frame(h2o.predict(mojo_model, df)))
   }
-  if ("classProbabilities" %in% names(output)) {
-    aux <- flatten_list(output$classProbabilities, quiet = TRUE)
-    colnames(aux) <- output$responseDomainValues[[1]]
-    output <- cbind(output[,c(1,2)], aux)
+  
+  if (method == "json") {
+    df <- as.data.frame(df)
+    df <- mutate_if(df, is.logical, as.character)
+    aux <- ceiling(nrow(df)/batch)
+    df$aux <- rep(1:aux, each = batch)[1:nrow(df)]
+    output <- c()
+    for (i in 1:aux) {
+      dfi <- select(df[df$aux == i,], -.data$aux)
+      json <- toJSON(dfi)
+      size <- nchar(json)
+      if (size > 250000)
+        stop(paste("JSON batch is too long. Please, try with a smaller 'batch' parameter.",
+                   "Suggested size:", round(batch * 235000 / size)))
+      res <- h2o.predict_json(zip, json, labels = TRUE)  
+      if ("error" %in% names(res)) {
+        message("\nERROR: There was an issue with one of the inputs to predict...")
+        writeLines(res$error)
+        break
+      }
+      output <- rbind(output, res)
+      if (aux > 1) statusbar(i, aux, i * batch)
+    }
+    if ("classProbabilities" %in% names(output)) {
+      aux <- flatten_list(output$classProbabilities, quiet = TRUE)
+      colnames(aux) <- output$responseDomainValues[[1]]
+      output <- cbind(output[,c(1,2)], aux)
+    }  
   }
   return(as_tibble(output))
 }
