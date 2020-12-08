a8915f76c7effb028cf202709c48bfdd2e16c6a7
Bernardo Lares <laresbernardo@gmail.com>
2020-12-08 18:44:31 -0300

    Welcome trim_mp3()

--- R/audio.R
+++ R/audio.R
@@ -12,11 +12,15 @@
 #' 
 #' @family Scrapper
 #' @family Audio
-#' @param id Characeter. YouTube URL or ID to search for
+#' @param id Character. YouTube URL or ID to search for.
 #' @param mp3 Boolean. Add mp3 optimal parameters?
-#' @param params Character. Additional parameters
+#' @param params Character. Additional parameters.
+#' @param start_time,end_time Numeric. Start and end time
+#' to trim the audio output in seconds.
+#' @param overwrite Boolean. Overwrite original file?
 #' @param info Boolean. Import and return metadata?
 #' @param cover Boolean. Google Search its squared cover?
+#' @param quiet Boolean. Keep quiet? If not, print messages.
 #' @examples 
 #' \dontrun{
 #'  # Download video from YouTube and convert to MP3
@@ -26,8 +30,12 @@
 get_mp3 <- function(id, 
                     mp3 = TRUE, 
                     params = "", 
+                    start_time = NA,
+                    end_time = NA,
+                    overwrite = TRUE,
                     info = TRUE,
-                    cover = FALSE) {
+                    cover = FALSE,
+                    quiet = FALSE) {
   
   # Build query's parameters
   query <- "--rm-cache-dir"
@@ -40,31 +48,12 @@ get_mp3 <- function(id,
   if (info) query <- c(query, '--write-info-json')
   query <- c(query, '-o "%(title)s.%(ext)s"', params)
   query <- v2t(c("youtube-dl", query, id), quotes = FALSE, sep = " ")
-  message(v2t(c("Query:", query), quotes = FALSE, sep = " "))
+  if (!quiet) message(v2t(c("Query:", query), quotes = FALSE, sep = " "))
   
   # Run youtube-dl  
   tryCatch({
     
     system(query)
-    
-    if (info) {
-      f <- listfiles(getwd(), recursive = FALSE) %>%
-        filter(grepl("\\.info\\.json", .data$filename)) %>%
-        arrange(desc(.data$mtime)) %>% .[1,1]
-      infox <- jsonlite::read_json(f)
-      invisible(file.remove(f)) 
-      infox[["formats"]] <- NULL
-    }
-    
-    if (cover & mp3 & info) {
-      aux <- gsub("\\.mp3", "", infox$title)
-      aux <- gsub("lyrics|lyric|official|video", "", tolower(aux))
-      aux <- gsub(" ", "\\+", cleanText(aux))
-      url <- glued("https://www.google.com/search?q={aux}&tbm=isch&tbs=iar%3As")
-      browseURL(url)
-    }
-    
-    return(invisible(infox))
     
   }, error = function(err) {
     msg <- "Something went wrong. Do you have youtube-dl installed?"
@@ -75,4 +64,54 @@ get_mp3 <- function(id,
                  "sudo pip3 install --upgrade youtube_dl", sep = "\n")
     stop(msg)
   })
+  
+  f <- listfiles(getwd(), recursive = FALSE) %>%
+    filter(grepl("\\.info\\.json", .data$filename)) %>%
+    arrange(desc(.data$mtime)) %>% .[1,1]
+  infox <- jsonlite::read_json(f)
+  invisible(file.remove(f)) 
+  infox[["formats"]] <- NULL
+  
+  if (cover & mp3 & info) {
+    aux <- gsub("\\.mp3", "", infox$title)
+    aux <- gsub("lyrics|lyric|official|video", "", tolower(aux))
+    aux <- gsub(" ", "\\+", cleanText(aux))
+    url <- glued("https://www.google.com/search?q={aux}&tbm=isch&tbs=iar%3As")
+    browseURL(url)
+  }
+  
+  # TRIM START AND/OR END OF AUDIO FILE
+  if (any(c(!is.na(start_time)), !is.na(end_time))) {
+    message(">>> Trimming audio file")
+    trim_mp3(sprintf("%s.mp3", infox$title), 
+             start_time, end_time, 
+             overwrite = FALSE,
+             quiet = quiet)
+    if (overwrite) file.remove(sprintf("%s.mp3", infox$title))
+  }
+  
+  return(invisible(infox))
+  
+}
+
+
+####################################################################
+#' Trim Audio File
+#' 
+#' This function trims MP3 files given a start and/or end numeric 
+#' timestamp. Requires \code{ffmpeg} installed in your machine.
+#' 
+#' @family Audio
+#' @inheritParams get_mp3
+#' @param file Character. File name to trim
+#' @export
+trim_mp3 <- function(file, start_time = 0, end_time, overwrite = FALSE, quiet = FALSE) {
+  start <- paste("-ss", start_time)
+  end <- ifelse(!is.na(end_time), paste("-to", end_time), "")
+  file_out <- ifelse(overwrite, file, paste0(gsub("\\.mp3.*","",file), "_trimmed.mp3"))
+  query2 <- paste("ffmpeg -hide_banner -loglevel panic -y", start, end, "-i", 
+                  sprintf("'%s'", file), 
+                  sprintf("'%s'", file_out))
+  if (!quiet) message("Query: ", query2)
+  system(query2)
 }
