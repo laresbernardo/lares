c88dba5b77d056b2780d28e6c8da3d25e2f0c7e1
laresbernardo <laresbernardo@gmail.com>
2024-12-12 19:56:07 +0100

    docs: improve docs for seq_surnames()

--- R/family.R
+++ R/family.R
@@ -1,14 +1,22 @@
 ####################################################################
-#' Family Tree Surnames Sequence
+#' Surnames Order Sequence
+#' 
+#' Generate a sequence of numbers that determines the order in which 
+#' surnames should be listed based on the number of generations of 
+#' ancestors you wish to include. This sequence follows the traditional 
+#' Latin custom of assigning the father's surname first, followed by 
+#' the mother's surname. The same logic extends systematically to higher 
+#' generations, ensuring that the order of surnames remains consistent 
+#' as you move upward through the family tree.
 #'
-#' @param n Integer. Number of generations. Notice this will generate sequences
-#' of 2^n integer values.
+#' @param n Integer. Number of generations to include in the sequence.
+#' Notice it will generate a vector with 2^(n-1) values.
 #' @return Integer vector.
 #' @examples
-#' seq_surnames(1) # Expected: 1
-#' seq_surnames(2) # Expected: 1, 2
-#' seq_surnames(3) # Expected: 1, 5, 3, 7, 2, 6, 4, 8
-#' seq_surnames(4) # Expected: 1, 9, 5, 13, 3, 11, 7, 15, 2, 10, 6, 14, 4, 12, 8, 16
+#' seq_surnames(1)
+#' seq_surnames(2)
+#' seq_surnames(3)
+#' seq_surnames(4)
 #' @export
 seq_surnames <- function(n = 1) {
   stopifnot(!is.integer(n))
