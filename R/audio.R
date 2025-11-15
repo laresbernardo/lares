9c72a4254f1de7a7390fb26ef1c759d11e3144a9
laresbernardo <laresbernardo@gmail.com>
2025-11-15 19:23:49 +0100

    feat: new title param for mp3_get()

--- R/audio.R
+++ R/audio.R
@@ -24,9 +24,12 @@
 #' @param open,delete Boolean. After everything's done, should the
 #' file be opened? Should it be deleted?
 #' @param info Boolean. Import and return metadata?
-#' @param cover Boolean. Google Search its squared cover?
+#' @param title Character. Title of the track to search for cover, metadata, and
+#' name the file.
+#' @param cover Boolean. Google Search its squared cover? Uses \code{title}
+#' input when provided.
 #' @param metadata Boolean. Use "spotifyr" to extract "track" data using Spotify's
-#' API. Needs credentials.
+#' API. Needs credentials. Uses \code{title} input when provided.
 #' @param quiet Boolean. Keep quiet? If not, informative messages will be shown.
 #' @return (Invisible) list with id's meta-data.
 #' @examples
@@ -50,6 +53,7 @@ mp3_get <- function(id,
                     open = FALSE,
                     delete = open,
                     info = TRUE,
+                    title = NULL,
                     cover = FALSE,
                     metadata = FALSE,
                     quiet = FALSE) {
@@ -68,7 +72,7 @@ mp3_get <- function(id,
   query <- c(query, '-o "%(title)s.%(ext)s"', params)
   query <- v2t(c(repo, query, id), quotes = FALSE, sep = " ")
   if (!quiet) message(v2t(c("Query:", query), quotes = FALSE, sep = " "))
-
+  
   # Run youtube-dl
   ret <- tryCatch(
     {
@@ -78,22 +82,22 @@ mp3_get <- function(id,
       msg <- "Something went wrong. Do you have youtube-dl installed?"
       if (grepl("^darwin", R.version$os)) {
         msg <- paste(msg, "Run in Terminal: brew install youtube-dl",
-          "Then restart and try again",
-          sep = "\n"
+                     "Then restart and try again",
+                     sep = "\n"
         )
       }
       msg <- paste(msg, "If already installed, try updating it with:",
-        "sudo pip3 install --upgrade youtube_dl",
-        sep = "\n"
+                   "sudo pip3 install --upgrade youtube_dl",
+                   sep = "\n"
       )
       msg <- paste(msg, "Our you could also try latest dev version that contains new features:",
-        "https://github.com/yt-dlp/yt-dlp/wiki/Installation",
-        sep = "\n"
+                   "https://github.com/yt-dlp/yt-dlp/wiki/Installation",
+                   sep = "\n"
       )
       stop(msg)
     }
   )
-
+  
   if (1 %in% ret) {
     invisible(NULL)
   } else {
@@ -101,13 +105,40 @@ mp3_get <- function(id,
       filter(grepl("\\.info\\.json", .data$filename)) %>%
       arrange(desc(.data$mtime)) %>%
       .[1, 1]
-
+    
     infox <- jsonlite::read_json(f)
     invisible(file.remove(f))
     infox[["formats"]] <- NULL
-    filename <- sprintf("%s.mp3", infox$title)
-    mp3_file <- gsub(".info.json", ".mp3", f)
-
+    
+    # The actual file path created by youtube-dl
+    mp3_file_original <- gsub(".info.json", ".mp3", f)
+    mp3_file <- mp3_file_original
+    
+    if (!is.null(title)) {
+      infox$title <- title
+      safe_title_for_file <- trimws(title)
+      safe_title_for_file <- gsub("\\s+", " ", safe_title_for_file)
+      new_mp3_file_base <- paste0(safe_title_for_file, ".mp3")
+      new_mp3_path <- file.path(dirname(mp3_file_original), new_mp3_file_base)
+      if (file.exists(mp3_file_original)) {
+        if (file.exists(new_mp3_path) && overwrite) {
+          file.remove(new_mp3_path)
+        } else if (file.exists(new_mp3_path) && !overwrite) {
+          if (!quiet) warning(paste("File already exists:", basename(new_mp3_path), "- skipping rename"))
+          mp3_file <- mp3_file_original 
+        }
+        if (file.exists(mp3_file_original) && !file.exists(new_mp3_path)) {
+          file.rename(mp3_file_original, new_mp3_path)
+          mp3_file <- new_mp3_path
+          if (!quiet) message(sprintf("File renamed to: %s", basename(mp3_file)))
+        }
+      } else {
+        if (!quiet) warning("Could not find the original MP3 file for renaming. Proceeding with original filename.")
+      }
+    }
+    
+    filename <- basename(mp3_file)
+    
     if (metadata) {
       try_require("spotifyr")
       message(">>> Adding metadata: ", infox$title)
@@ -123,11 +154,11 @@ mp3_get <- function(id,
       ) %>%
         rowwise() %>%
         mutate(matching = grepl(.data$name, infox$title) |
-               grepl(.data$album.name, infox$title)) %>%
+                 grepl(.data$album.name, infox$title)) %>%
         arrange(desc(.data$matching)) %>%
         head(1)
       album <- get_album(sp$album.id, authorization = authorization)
-
+      
       # Fetch (first) artist's genre if no genre album genres available
       if (length(album$genres) == 0) {
         artist <- search_spotify(
@@ -136,28 +167,27 @@ mp3_get <- function(id,
           authorization = authorization
         )
         genres <- cleanText(artist$genres[[1]][1],
-          spaces = TRUE, keep = c("&", "/"), title = TRUE
+                            spaces = TRUE, keep = c("&", "/"), title = TRUE
         )
         if (genres == "Null") genres <- ""
       } else {
         genres <- v2t(album$genres, sep = " / ")
         artist <- NULL
       }
-
+      
       # Update all metadata
       infox$audio <- mp3_update_tags(
         mp3_file,
         title = sp$name,
         artist = v2t(sp$artists[[1]]$name, quotes = FALSE),
         album = sp$album.name,
-        # release_date = lubridate::year(sp$album.release_date),
         release_date = sp$album.release_date,
         recording_date = sp$album.release_date,
         track_num = tuple(count = sp$track_number, total = sp$album.total_tracks),
         genre = genres,
         label = album$label
       )
-
+      
       # Handle cover art from URL
       cover_url <- sp$album.images[[1]]$url[1]
       if (!is.null(cover_url)) {
@@ -171,7 +201,7 @@ mp3_get <- function(id,
       }
       infox$metadata <- list(track = sp, artist = artist, album = album)
     }
-
+    
     if (cover && mp3 && info) {
       aux <- gsub("\\.mp3", "", infox$title)
       aux <- gsub("lyrics|lyric|official|video", "", tolower(aux))
@@ -179,20 +209,19 @@ mp3_get <- function(id,
       url <- glued("https://www.google.com/search?q={aux}&tbm=isch&tbs=iar%3As")
       browseURL(url)
     }
-
+    
     # TRIM START AND/OR END OF AUDIO FILE
     if (any(c(start_time > 0, !is.na(end_time)))) {
       message(">>> Trimming audio file: ", filename)
-      mp3_trim(file,
-        start_time = start_time,
-        end_time = end_time,
-        overwrite = overwrite,
-        quiet = quiet
+      mp3_trim(file = mp3_file,
+               start_time = start_time,
+               end_time = end_time,
+               overwrite = overwrite,
+               quiet = quiet
       )
     }
-
+    
     # Open file once everything is done
-    mp3_file <- gsub(".info.json", ".mp3", f)
     if (open) {
       if (file.exists(mp3_file)) {
         message("Opening file: ", mp3_file)
@@ -270,26 +299,25 @@ mp3_trim <- function(file, start_time = 0, end_time = NA,
 #' @param album Character. New album name (optional).
 #' @param genre Character. New genre name (optional).
 #' @param ... Additional named arguments corresponding to other ID3 tags.
-#'   Only tags that exist in the MP3 file will be updated.
+#' Only tags that exist in the MP3 file will be updated.
 #' @examples
 #' \dontrun{
 #' mp3_update_tags(
-#'   "song.mp3",
-#'   title = "My Jazz Song",
-#'   artist = "Bernardo",
-#'   album = "Smooth Album",
-#'   genre = "Jazz"
+#' "song.mp3",
+#' title = "My Jazz Song",
+#' artist = "Bernardo",
+#' album = "Smooth Album",
+#' genre = "Jazz"
 #' )
 #' }
 #' @return Invisibly returns \code{NULL}. The MP3 file is updated in-place.
 #' @export
 mp3_update_tags <- function(filename, title = NULL, artist = NULL, album = NULL, genre = NULL, ...) {
-  # filename <- "Steve Kroeger x Life of Kai - Summer (Lyrics).mp3"
   if (!file.exists(filename)) {
     message("File does not exist. Check filename input and working directory....")
   } else {
     try_require("reticulate")
-    eyeD3 <- NULL # To avoid check() issues
+    eyeD3 <- NULL
     eyeD3 <- import("eyed3")
     audio <- eyeD3$load(normalizePath(filename))
     if (is.null(audio$tag)) audio$initTag()
@@ -297,7 +325,6 @@ mp3_update_tags <- function(filename, title = NULL, artist = NULL, album = NULL,
     valid_tags <- names(audio$tag)
     for (n in names(tag_args)) {
       if (!is.null(tag_args[[n]]) && n %in% valid_tags) {
-        # message("... ", n)
         audio$tag[[n]] <- tag_args[[n]]
       }
     }
